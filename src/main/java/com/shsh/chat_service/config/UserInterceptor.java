3d41d4af93c5fb92862ea5a24d196e180425f222
Daniil Grishenok <grishenokdaniil5@gmail.com>
2024-08-24 23:40:36 +0300

    refactor: удалены лишние импорты

--- src/main/java/com/shsh/chat_service/config/UserInterceptor.java
+++ src/main/java/com/shsh/chat_service/config/UserInterceptor.java
@@ -1,21 +1,16 @@
 package com.shsh.chat_service.config;
 
 import com.shsh.chat_service.model.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
-
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
+@Log4j2
 public class UserInterceptor implements ChannelInterceptor {
 
     @Override
@@ -24,6 +19,7 @@ public class UserInterceptor implements ChannelInterceptor {
 
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             String userId = accessor.getFirstNativeHeader("X-User-Id");
+            log.info("X-User-Id inceraptor:  " + userId);
             if (userId != null) {
                 accessor.setUser(new User(userId));
             }
